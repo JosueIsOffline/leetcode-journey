c316e921f1160fb2813f8663c14979fd68aba4dc
JosueIsOffline <josuehernandez2314@gmail.com>
2025-11-26 00:40:18 -0400

    feat: 001-two-sum completed

--- solutions/easy/001-two-sum/solution.js
+++ solutions/easy/001-two-sum/solution.js
@@ -1,1 +1,18 @@
-console.log("Hello from js");
+function twoSum(nums, target) {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    console.log(complement);
+
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+
+    map.set(nums[i], i);
+  }
+
+  return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
--- solutions/easy/001-two-sum/solution.py
+++ solutions/easy/001-two-sum/solution.py
@@ -1,1 +1,15 @@
-print("Hello python")
+def twoSum(nums: list[int], target: int) -> list[int]:
+    map = {}
+
+    for i in range(len(nums)):
+        complement = target - nums[i]
+
+        if complement in map:
+            return [map[complement], i]
+
+        map[nums[i]] = i
+
+    return []
+
+
+print(twoSum([1, 5, 6, 3], 4))
